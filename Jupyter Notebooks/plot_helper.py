dbdb7ae46b23b464df1de3c9caf6e2bd413f4642
Moses Won <momopeachboy026@gmail.com>
2019-04-09 13:59:46 -0700

    Tried edge case

---
+++ Jupyter Notebooks/plot_helper.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_histogram(data, bin_size, nominal_value=None):
+    hist, edges = np.histogram(data, bin_size)
+    edge_means = [(edges[i] + edges[i+1])/2 for i in range(len(edges) -1)]
+    fig, ax = plt.subplots()
+    ax.plot(edge_means, hist)
+    if nominal_value is not None:
+        line_height = max(hist)
+        ax.plot([nominal_value] * line_height, range(line_height))
