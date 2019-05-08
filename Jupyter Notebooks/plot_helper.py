376e6b07e2e5a300bb030c502ea16394ee85bd56
Moses Won <momopeachboy026@gmail.com>
2019-05-08 00:16:54 -0700

    new notebooks

--- Jupyter Notebooks/plot_helper.py
+++ Jupyter Notebooks/plot_helper.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_histogram(data, bin_size, xlim, nominal_value=None):
+def plot_histogram(data, bin_size, xlim=None, nominal_value=None):
     hist, edges = np.histogram(data, bin_size)
     edge_means = [(edges[i] + edges[i+1])/2 for i in range(len(edges) -1)]
     fig, ax = plt.subplots()
@@ -9,4 +9,4 @@ def plot_histogram(data, bin_size, xlim, nominal_value=None):
     if nominal_value is not None:
         line_height = max(hist)
         ax.plot([nominal_value] * line_height, range(line_height))
-        ax.set_xlim(left=xlim[0], right=xlim[1])
+        #ax.set_xlim(left=xlim[0], right=xlim[1])
